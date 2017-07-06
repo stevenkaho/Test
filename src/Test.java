15c75da15ef72586114b538014e93bdce0802299
stevenkaho <stevenbusiness998@gmail.com>
2017-07-06 21:28:25 +0800

    added pic

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,15 @@
 
-import java.awt.Image; 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
 
 import javax.swing.ImageIcon; 
 import javax.swing.JFrame; 
-import javax.swing.JLabel; 
+import javax.swing.JLabel;
+import javax.swing.JOptionPane; 
 
 public class Test extends JFrame { 
 
@@ -14,16 +20,38 @@ public class Test extends JFrame {
 
 public Test() { 
 
- int width = 100; 
- int height = 100; 
-  
- ImageIcon image = new ImageIcon("c:\\Users\\StevenCheng\\workspace\\Test\\src\\Brick.JPG"); 
- image.setImage(image.getImage().getScaledInstance(width,height,Image.SCALE_DEFAULT)); 
- JLabel label = new JLabel(image); 
-  
- getContentPane().add(label); 
-  
- setBounds(100, 100, 200, 200); 
+ int width = 30; 
+ int height = 30; 
+ String FilePath = "c:\\Users\\StevenCheng\\workspace\\Test\\src\\";
+
+ String[] imagelists = new String[9];
+ // fill up imagelists with File string
+ for(int i =0 ; i < imagelists.length ; i++ ){
+	 imagelists[i] = FilePath  + Integer.toString(i) + ".jpg";
+ }
+ ImageIcon [] imgUP = new ImageIcon[9]; // image array
+ JLabel jl[] = new JLabel[9]; // label array
+/* 
+ Container c;
+ c = this.getContentPane();
+ setSize(800,600);
+*/ 
+ this.setResizable(false);//set resize button disable
+ 
+ setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ setLayout(new GridLayout(3,3));
+ 
+
+ for (int i =0; i < imagelists.length ; i++){
+	imgUP[i] = new ImageIcon(imagelists[i]);
+ 	imgUP[i].setImage(imgUP[i].getImage().getScaledInstance(width,height,Image.SCALE_DEFAULT));
+ 	jl[i] = new JLabel(imgUP[i]);
+ 	getContentPane().add(jl[i]);
+ 	jl[i].setBounds(new Rectangle(new Point(50,50), jl[i].getPreferredSize()));
+ };
+
+ 
+ setBounds(100, 100, 400, 400); 
  setDefaultCloseOperation(EXIT_ON_CLOSE); 
  setVisible(true); 
  } 
